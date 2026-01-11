53806576f1d4da7e547d115153b0c10e30344118
Ast <svyryd.andriy@gmail.com>
2026-01-11 17:57:33 +0200

    Add environmentInfo stage to display environment variables and tool versions

---
+++ vars/environmentInfo.groovy
@@ -0,0 +1,16 @@
+def call() {
+    stage('Environment info') {
+        // In shared libs, use `script {}` to safely access env vars and run Groovy logic
+        script {
+            echo "${env.APP_GREETING} - Environment: ${env.ENV}"
+            echo "Node name: ${env.NODE_NAME}"
+            echo "Workspace: ${env.WORKSPACE}"
+            echo "BRANCH_NAME=${env.BRANCH_NAME}"
+            echo "GIT_BRANCH=${env.GIT_BRANCH}"
+            echo "Building ${env.JOB_NAME} #${env.BUILD_NUMBER}"
+        }
+
+        sh 'java -version'
+        sh 'mvn -version'
+    }
+}
